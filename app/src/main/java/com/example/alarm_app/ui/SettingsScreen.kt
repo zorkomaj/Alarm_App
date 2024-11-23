b1648f4182ce1b3b4ffce2b6836a4360247248e6
Maj <zorko.maj@gmail.com>
2024-11-23 14:29:40 +0100

    Revert "Made Maps & Settings"
    This reverts commit 759d398b484f2c8bd74843fc212f0a8ebd2dd342.

--- app/src/main/java/com/example/alarm_app/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/alarm_app/ui/SettingsScreen.kt
@@ -1,88 +1,20 @@
 package com.example.alarm_app.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Switch
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier) {
-    val settingsList = listOf("Setting 1", "Setting 2", "Setting 3")
-    val checkedStates = remember { mutableStateListOf(true, true, true) }
-
-    Column(modifier = modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            items(settingsList) { setting ->
-                val index = settingsList.indexOf(setting)
-                SettingRow(
-                    settingName = setting,
-                    checked = checkedStates[index],
-                    onCheckedChange = {
-                        checkedStates[index] = it
-                    }
-                )
-            }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Button(
-                onClick = {  },
-                modifier = Modifier.padding(end = 8.dp)
-            ) {
-                Text("Import")
-            }
-            Button(
-                onClick = {  },
-                modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Text("Export")
-            }
-        }
-    }
-}
-
-@Composable
-fun SettingRow(settingName: String, checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
-    Row(
-        modifier = Modifier
-            .padding(16.dp, 16.dp, 16.dp, 16.dp)
-            .background(Color(0xd1d1d1ff), shape = RoundedCornerShape(20.dp))
-            .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Text(
-                text = settingName,
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
-            )
-        }
-
-        Switch(
-            checked = checked,
-            onCheckedChange = onCheckedChange,
-            modifier = Modifier.padding(start = 50.dp)
-        )
+        Text(text = "Settings")
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,3 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
-
-buildscript {
-    dependencies {
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
-}
