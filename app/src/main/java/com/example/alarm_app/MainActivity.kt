c849f9e228c692b288f835d0b86a134d3990c6fb
ZanAmbrozic <zan.ambrozic@gmail.com>
2025-01-14 23:21:16 +0100

    Added lifecycle logging

--- app/src/main/java/com/example/alarm_app/MainActivity.kt
+++ app/src/main/java/com/example/alarm_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.alarm_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,5 +16,25 @@ class MainActivity : ComponentActivity() {
                 MainScreen(context = this)
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivityLifecycle", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivityLifecycle", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivityLifecycle", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivityLifecycle", "onStop called")
     }
 }
